2fbc318e6a2be1f60035838a9637ac25ece18927
Dave Yates <dave@upnorthcreativemedia.com>
2018-04-08 10:19:44 +0100

    Aded mocha tests

---
+++ test/bitsofcoin.js
@@ -0,0 +1,25 @@
+const assert = require('assert')
+
+//Begin bitsofcoin test suite.
+describe('bitsofcoin API', () => {
+  // 1. Get full chain
+  describe('/getChain', () => {
+    it('Should return a chain with only the genesis block', () => {
+
+
+    })
+  })
+  // 2. Mine a new block.
+  describe('/mine', () => {
+    it('Should mine a new block to add into the chain', () => {
+
+    })
+  })
+
+  // 3. Add new transaction to the blockchain.
+  describe('/transaction/new', () => {
+    it('Should add the new transaction to the next mining', () => {
+
+    })
+  })
+})
