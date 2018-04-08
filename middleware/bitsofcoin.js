1fc1309d2279207f2b29669755e346e6860e79de
Dave Yates <dave@upnorthcreativemedia.com>
2018-04-08 22:34:02 +0100

    Mine test Passing

--- middleware/bitsofcoin.js
+++ middleware/bitsofcoin.js
@@ -15,7 +15,24 @@ class BitsOfCoin{
 		}
     	return next()
 	}
-	mine(req, res, next){}
+	mine(req, res, next){
+		const lastBlock = this.blockchain.lastBlock()
+	    const lastProof = lastBlock.proof
+	    const proof = this.blockchain.proofOfWork(lastProof)
+
+	    // Create a new transaction with from 0 (this node) to our node (NODE_NAME) of 1 BitsOfCoin
+	    this.blockchain.newTransaction('0', process.env.NODE_NAME, 1)
+
+	    // Forge the new Block by adding it to the chain
+	    const previousHash = this.blockchain.hash(lastProof)
+	    const newBlock = this.blockchain.newBlock(proof, previousHash)
+
+	    const responseValue = Object.assign({
+	      message: 'New Block mined'
+	    }, newBlock)
+	    req.responseValue = responseValue
+	    return next()
+	}
 	newTransaction(req, res, next){}
 }
 
